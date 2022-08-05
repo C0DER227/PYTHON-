0f16ba825c552f1221d482861a2f8b0cee4651c7
C0DER227 <110673821+C0DER227@users.noreply.github.com>
2022-08-05 20:02:45 +0530

    Variables and datatypes

---
+++ variables&datatypes/calc.py
@@ -0,0 +1,14 @@
+a=input("enter the first number:")
+b=input("enter the second number:")
+a=int(a)
+b=int(b)
+A=a+b
+S=a-b
+M=a*b
+D=a/b
+compare=(a>b)
+print("addition of two numbers is:",A)
+print("substraction of two numbers is :",S)
+print("multiplication of two numbers is:",M)
+print("division of two numbers is:",D)
+print(compare)
---
+++ variables&datatypes/datatypes.py
@@ -0,0 +1,7 @@
+a="3500"
+b= 450
+c= 56.90
+print(b>c)
+print(type(a))
+print(type(b))
+print(type(c))
